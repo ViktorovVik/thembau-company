80f702e7f83bc0da4a61a50fb8f23f0bdb03cbbd
Viktor Shuntikov <schuntikov.viktor@yandex.ru>
2025-06-04 00:34:12 +0300

    complete header with swiper

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 import {MobileMenu} from './MobileMenu.js'
 
-
 const mobileNav = new MobileMenu();
+
+const swiperHEader = new Swiper('.swiper-header', {
+   loop: true,
+   speed: 1000,
+   keyboard: {
+      enabled: true
+   },
+   slidesPerView: 1,
+   spaceBetween: 0,
+
+   pagination: {
+      el: '.header-bottom__slider-controls-count',
+      type: 'fraction',
+
+      formatFractionTotal: function (total) {
+        return String(total).padStart(2, '0');
+      },
+      formatFractionCurrent: function (current) {
+        return String(current).padStart(2, '0');
+      },
+      renderFraction: function (currentClass, totalClass) {
+        return '<span class="' + currentClass + '"></span>' +
+               '<span class="custom-swiper-line"></span>' + 
+               '<span class="' + totalClass + '"></span>';
+      }
+   },
+
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+});
+
